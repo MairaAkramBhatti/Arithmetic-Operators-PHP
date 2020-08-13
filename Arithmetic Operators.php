dea4feec5bdf33fe247d9b266ba43c372c485c59
Maira Akram <59651048+MairaAkramBhatti@users.noreply.github.com>
2020-08-14 01:41:19 +0500

    Arithmetic Operators
    Operators are explain

---
+++ Arithmetic Operators.php
@@ -0,0 +1,91 @@
+<html>
+<body>
+<?php
+echo"<h2> PHP Arithmetic Operators <h2>";
+
+//Addition
+
+$a = 5;
+$b = 5;
+
+$c = $a + $b ;
+
+echo "Adition<br>";
+
+echo "Frist Value ".$a."<br>";
+echo "Second value  ".$b. "<br>";
+
+echo "Answer is ".$c."<br><br><br>";
+
+//Subtraction
+
+$a = 5;
+$b = 5;
+
+$c = $a - $b ;
+
+echo "Subtraction<br>";
+
+echo "Frist Value ".$a."<br>";
+echo "Second value  ".$b. "<br>";
+
+echo "Answer is ".$c."<br><br><br>";
+
+//Multipication
+
+$a = 5;
+$b = 5;
+
+$c = $a * $b ;
+
+echo "Multiply<br>";
+
+echo "Frist Value ".$a."<br>";
+echo "Second value  ".$b. "<br>";
+
+echo "Answer is ".$c."<br><br><br>";
+
+
+//Division
+
+$a = 5;
+$b = 5;
+
+$c = $a / $b ;
+
+
+echo"Division<br>";
+echo "Frist Value ".$a."<br>";
+echo "Second value  ".$b. "<br>";
+
+echo "Answer is ".$c."<br><br><br>";
+
+$a = 5;
+$b = 6;
+
+//  Modules
+
+$c = $a % $b ;
+
+echo"Modules<br>";
+echo "Frist Value ".$a."<br>";
+echo "Second value  ".$b. "<br>";
+
+echo "Answer is ".$c."<br><br><br>";
+
+$a = 5;
+$b = 6;
+
+//	Exponentiation
+
+$c = $a ** $b ;
+
+echo"Exponentiation<br>";
+echo "Frist Value ".$a."<br>";
+echo "Second value  ".$b. "<br>";
+
+echo "Answer is ".$c."<br><br><br>";
+
+?>  
+</body>
+</html>
